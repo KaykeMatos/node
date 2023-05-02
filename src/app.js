74179257013e75f97ae63f78f849f5ba3c40b61e
kayke <kaykebritomatos@hotmail.com>
2023-05-02 15:12:18 -0300

    Crud em Nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from "express";
-import db from "./config/db.Connect.js"
-import livros from "./models/Livro.js";
-import model from "mongoose";
+import db from "./config/db.Connect.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
@@ -14,53 +12,5 @@ app.use(express.json())
 
 routes(app);
 
-/*const livros = [
-        { id: 1, "titulo": "Harry potter e a Pedra filosofal" },
-        { id: 2, "titulo": "Harry potter e a Camara secreta" }
-    ]
-*/
-
-
-//Crud /Livros
-
-
-
-
-
-
-//Get para Id específico
-app.get('/livros/:id', (req, res) => {
-        let index = buscaLivro(req.params.id);
-        res.json(livros[index]);
-
-    })
-    // Criar novos id's
-app.post('/livros', (req, res) => {
-        livros.push(req.body);
-        res.status(201).send('Livro foi cadastrado com sucesso!')
-    })
-    // Atualizar id's existentes
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-
-})
-
-// Deletar Id's existentes
-
-app.delete('/livros/:id', (req, res) => {
-    let { id } = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} foi removido com sucesso!`)
-
-
-})
-
-
-function buscaLivro(id) {
-    return livros.findIndex(livros => livros.id == id)
-}
 
 export default app;
